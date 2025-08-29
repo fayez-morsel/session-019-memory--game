2cac45017e9d3583a0ae80f0727175ad5243507c
Fayez Morsel <fayezmorsel77@gmail.com>
2025-08-29 16:37:43 +0300

    player Name setup

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const currentLevelText = document.getElementById("currentLevel");
 const bestScoreText = document.getElementById("bestScore");
 const playerNameText = document.getElementById("playerName");
 const buttons = document.querySelectorAll(".btn");
+
+let playerName = localStorage.getItem("playerName");
+if (!playerName) {
+  playerName = prompt("Enter your name:") || "Player";
+  localStorage.setItem("playerName", playerName);
+}
+playerNameText.textContent = "👾 " + playerName;
